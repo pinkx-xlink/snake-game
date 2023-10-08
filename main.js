898f56e7baec84cf30dd8f8c69a51ffacd541a9b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-10-08 14:01:15 -0400

    Add true & false cases for if snake "bites itself"

--- main.js
+++ main.js
@@ -65,5 +65,15 @@ function moveSnake(squares) {
 }
 
 function checkForHits(squares) {
-    
+    if (
+        (currentSnake[0] + width >= width * width && direction === width) ||
+        (currentSnake[0] % width === width + 1 && direction === 1) || 
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (currentSnake[0] - width <= 0 && direction === -width) ||
+        squares[currentSnake[0] + direction].classList.contains("snake")
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }

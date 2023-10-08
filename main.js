3f42a6d7fcbdd3e5adc0ef2e0c394333b00a9a43
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-10-08 14:08:38 -0400

    Finish EatApple() -> logs when player hits apple

--- main.js
+++ main.js
@@ -79,5 +79,15 @@ function checkForHits(squares) {
 }
 
 function eatApple(squares, tail) {
-    
+    if (squares[currentSnake[0]].classList.contains("apple")) {
+        squares[currentSnake[0]].classList.remove("apple");
+        squares[tail].classList.add("snake");
+        currentSnake.push(tail);
+        randomApple(squares);
+        score++;
+        scoreDisplay.textContent = score;
+        clearInterval(interval);
+        intervalTime = intervalTime * speed;
+        interval = setInterval(moveOutcome, intervalTime);
+    }
 }

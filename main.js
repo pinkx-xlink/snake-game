8d317de81a439418540a8aa20a79e2e18f1396b4
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-10-08 13:33:19 -0400

    Add alert for if player hits something

--- main.js
+++ main.js
@@ -45,5 +45,12 @@ function startGame() {
 }
 
 function moveOutcome() {
-    
+    let squares = document.querySelectorAll(".grid div");
+    if (checkForHits(squares)) {
+        alert("you hit something!");
+        popup.style.display = "flex";
+        return clearInterval(interval);
+    } else {
+        moveSnake(squares);
+    }
 }

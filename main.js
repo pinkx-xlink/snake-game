e049a38fe18f743259e4121a9718a6fca3f7c462
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-10-08 14:12:32 -0400

    Fix typo line 99

--- main.js
+++ main.js
@@ -96,5 +96,5 @@ function randomApple(squares) {
     do {
         appleIndex = Math.floor(Math.random() * squares.length);
     } while (squares[appleIndex].classList.contains(snake));
-    squaers[appleIndex].classList.add("apple");
+    squares[appleIndex].classList.add("apple");
 }

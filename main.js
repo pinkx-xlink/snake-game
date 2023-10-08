8f11b23d2cc085808f3fe417a7f1553981283d03
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-10-08 13:24:40 -0400

    Complete  startGame()

--- main.js
+++ main.js
@@ -32,5 +32,14 @@ function createBoard() {
 }
 
 function startGame() {
-    
+    let squares = document.querySelectorAll(".grid div");
+    randomApple((squares);
+    //random apple
+    direction = 1;
+    scoreDisplay.innerHTML = score;
+    intervalTime = 1000;
+    currentSnake = [2, 1, 0];
+    currentIndex = 0;
+    currentSnake.forEach((index) => squares[index].classList.add("snake"));
+    interval = setInterval(moveOutcone, intervalTime);
 }
